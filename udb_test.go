f2c18894b9bf32f1ccee96de501f8616ed15a4d0
ego008 <ego008@gmail.com>
2021-04-21 23:20:10 +0800

    Update udb_test.go

--- udb_test.go
+++ udb_test.go
@@ -19,17 +19,7 @@ func init() {
 
 func TestTx_Hset(t *testing.T) {
 	err = db.Update(func(tx *bolt.Tx) error {
-		db.Tx = tx
-		return db.Hset(bucket, []byte("key1"), []byte("value1"))
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestTx_Hset2(t *testing.T) {
-	err = db.Update2(func() error {
-		return db.Hset(bucket, []byte("key1"), []byte("value1"))
+		return db.Hset(tx, bucket, []byte("key1"), []byte("value1"))
 	})
 	if err != nil {
 		t.Fatal(err)
@@ -38,21 +28,7 @@ func TestTx_Hset2(t *testing.T) {
 
 func TestTx_Hget(t *testing.T) {
 	err = db.View(func(tx *bolt.Tx) error {
-		db.Tx = tx
-		rs := db.Hget(bucket, []byte("key1"))
-		if rs.String() != "value1" {
-			return errors.New(" Hget err")
-		}
-		return nil
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestTx_Hget2(t *testing.T) {
-	err = db.View2(func() error {
-		rs := db.Hget(bucket, []byte("key1"))
+		rs := db.Hget(tx, bucket, []byte("key1"))
 		if rs.String() != "value1" {
 			return errors.New(" Hget err")
 		}
@@ -65,8 +41,7 @@ func TestTx_Hget2(t *testing.T) {
 
 func TestTx_Hscan(t *testing.T) {
 	err = db.View(func(tx *bolt.Tx) error {
-		db.Tx = tx
-		rs := db.Hscan(bucket, nil, 2)
+		rs := db.Hscan(tx, bucket, nil, 2)
 		if !rs.OK() || rs.KvLen() != 1 {
 			return errors.New(" Hscan err")
 		}
@@ -77,36 +52,9 @@ func TestTx_Hscan(t *testing.T) {
 	}
 }
 
-func TestTx_Hset_TxNil(t *testing.T) {
-	err = db.Update(func(tx *bolt.Tx) error {
-		return db.Hset(bucket, []byte("key1"), []byte("value1"))
-	})
-	if err != ErrTxClosed {
-		t.Fatal(err)
-	}
-}
-
-func TestTx_Hset_TxNil2(t *testing.T) {
-	err = db.Hset(bucket, []byte("key1"), []byte("value1"))
-	if err != ErrTxClosed {
-		t.Fatal(err)
-	}
-}
-
-func TestTx_Hset_TxNotWritable(t *testing.T) {
-	err = db.View(func(tx *bolt.Tx) error {
-		db.Tx = tx
-		return db.Hset(bucket, []byte("key1"), []byte("value1"))
-	})
-	if err != bolt.ErrTxNotWritable {
-		t.Fatal(err)
-	}
-}
-
 func TestTx_Zset(t *testing.T) {
 	err = db.Update(func(tx *bolt.Tx) error {
-		db.Tx = tx
-		return db.Zset(bucket, []byte("key1"), 2)
+		return db.Zset(tx, bucket, []byte("key1"), 2)
 	})
 	if err != nil {
 		t.Fatal(err)
@@ -115,8 +63,7 @@ func TestTx_Zset(t *testing.T) {
 
 func TestTx_Zget(t *testing.T) {
 	err = db.View(func(tx *bolt.Tx) error {
-		db.Tx = tx
-		rs := db.Zget(bucket, []byte("key1"))
+		rs := db.Zget(tx, bucket, []byte("key1"))
 		if rs.Uint64() != 2 {
 			return errors.New(" Zget err")
 		}
